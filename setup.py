aaec71592150bafee228ec1d214792313c39d7e1
Sean Robertson <sdrobert@cs.toronto.edu>
2018-11-16 17:50:42 -0500

    Fixing up setup.py classsifiers

--- setup.py
+++ setup.py
@@ -34,15 +34,14 @@ setup(
     packages=['pydrobert', 'pydrobert.gpyopt'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Researchers',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         'numpy', 'future', 'GPyOpt', 'param', 'matplotlib'
